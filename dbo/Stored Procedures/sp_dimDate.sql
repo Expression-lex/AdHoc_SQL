d87cfbd7c63f0973c24d5cc8eee896941c4562fe
Alex Ablanya <harlex1990@gmail.com>
2024-02-05 20:47:55 +0100

    Add project files.

---
+++ dbo/Stored Procedures/sp_dimDate.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE sp_dimDate (
+	@DayNumberOfWeek		int
+)
+as
+begin
+
+SELECT  [DateKey]
+      ,[FullDateAlternateKey]
+      ,[DayNumberOfWeek]
+      ,[EnglishDayNameOfWeek]
+      ,[SpanishDayNameOfWeek]
+      ,[FrenchDayNameOfWeek]
+      ,[DayNumberOfMonth]
+      ,[DayNumberOfYear]
+      ,[WeekNumberOfYear]
+      ,[EnglishMonthName]
+      ,[SpanishMonthName]
+      ,[FrenchMonthName]
+      ,[MonthNumberOfYear]
+      ,[CalendarQuarter]
+      ,[CalendarYear]
+      ,[CalendarSemester]
+      ,[FiscalQuarter]
+      ,[FiscalYear]
+      ,[FiscalSemester]
+  FROM [AdventureWorksDW2016].[dbo].[DimDate]
+  where [DayNumberOfWeek] = @DayNumberOfWeek	
+  end
